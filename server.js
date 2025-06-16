453e46d07c42c0ca068a0dbca0ca6f04b377b7b2
imarslan444 <151779880+imarslan44@users.noreply.github.com>
2025-06-16 12:25:26 +0530

    debuged prot variable

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ dotenv.config({path: './.env'});
 import { connectDB } from './config/db.js';
 import productRoutes from './Routes/product.routes.js';
 import cors from 'cors';
-
-
 const app = express();
+
 app.use(cors({
-  origin: "https://product-store-4000.netlify.app/", // Allow requests from this origin
+  origin: "*", // Allow requests from this origin
+
 })); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
 // app.use(express.static('dist')); // Serve static files from the 'public' directory
 //get all products
 
 app.use('/api/products', productRoutes);
+const PORT = process.env.PORT || 4000;
 
 
 
 
-
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
 
   connectDB()
-  console.log('Server is running on port http://localhost:' + process.env.PORT + "/api/products");
+  console.log('Server is running on port http://localhost:' + PORT + "/api/products");
 
 });
 
